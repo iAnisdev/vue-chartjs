74ef4ded5927a3078004d8984a191519e3bfb7fe
marahmanjs <imarahmandev@gmail.com>
2022-01-14 22:00:05 +0500

    v1 complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="!isProcessing">
+    <bar-chart :data="barChartData" :options="barChartOptions" :height="'100%'" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import barChart from "./components/BarChart";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    barChart,
+  },
+  data() {
+    return {
+      normalDataset: [
+        {
+          team: "arsenal",
+          goals: 50,
+        },
+        {
+          team: "spurs",
+          goals: 5,
+        },
+        {
+          team: "wigan",
+          goals: 300,
+        },
+        {
+          team: "oxford united",
+          goals: 1200,
+        },
+      ],
+      isProcessing: true,
+      barChartData: {
+      },
+      barChartOptions: {
+        type: 'horizontalBar',
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          fontSize: 24,
+          fontColor: "#6b7280",
+        },
+        tooltips: {
+          backgroundColor: "#17BF62",
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    };
+  },
+  methods: {
+    parseData(normalDataset) {
+      this.isProcessing = true
+      let barChartData = {
+        labels: [],
+          datasets: [
+            {
+              data: [],
+              backgroundColor: "#F9B2C3",
+            },
+          ],
+      };
+      normalDataset.forEach((element) => {
+        barChartData.labels.push(element.team)
+        barChartData.datasets[0].data.push(element.goals)
+      });
+      this.barChartData = barChartData
+      setTimeout(() => {
+        this.isProcessing = false
+      }, 100);
+    },
+  },
+  mounted() {
+    this.parseData(this.normalDataset);
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
